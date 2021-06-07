65ba5c9f539e245bbdab59b8edbff446927a9d7f
Benjamin BRASSART <bbrassar@e3r9p8.42.fr>
2021-06-07 10:30:38 +0200

    Added ft_memmove and ft_memset

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/07 10:11:48 by bbrassar          #+#    #+#             */
+/*   Updated: 2021/06/07 10:30:23 by bbrassar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 16
+# endif
+
+# include <string.h>
+
+int		get_next_line(int fd, char **line);
+
+void	*ft_memmove(void *dst, void const *src, size_t n);
+
+void	*ft_memset(void *s, int c, size_t n);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/07 10:24:28 by bbrassar          #+#    #+#             */
+/*   Updated: 2021/06/07 10:30:12 by bbrassar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_memmove(void *dst, void const *src, size_t n)
+{
+	unsigned char		*d;
+	unsigned char const	*s;
+	size_t				i;
+
+	d = dst;
+	s = src;
+	i = 0;
+	while (i < n)
+	{
+		if (d < s)
+			d[n - i - 1] = s[n - i - 1];
+		else
+			d[i] = s[i];
+		++i;
+	}
+	return (dst);
+}
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	ch;
+
+	d = s;
+	ch = c;
+	while (n--)
+		*d++ = ch;
+	return (s);
+}

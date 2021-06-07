fb57475a8bcc104db91f9cae4d95ccb005a2f7f9
Benjamin BRASSART <bbrassar@e3r9p8.42.fr>
2021-06-07 10:55:21 +0200

    Added gnl_strjoin to get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 10:11:48 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/06/07 10:30:23 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/06/07 10:35:37 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 # endif
 
 # include <string.h>
+
+int		gnl_strjoin(char **line, char *s, size_t n);
 
 int		get_next_line(int fd, char **line);
 
